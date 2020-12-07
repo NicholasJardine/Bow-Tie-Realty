7565689ba344b3aba5e6e9624b54636bc9603b28
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-12-07 13:13:05 +0200

    user and listing models updated

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,14 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
+  has_one_attached :pic1
+  has_one_attached :pic2
+  has_one_attached :pic3
+  has_one_attached :pic4
+  has_one_attached :pic5
+  has_one_attached :pic6
+  has_one_attached :pic7
+  has_one_attached :pic8
+  has_one_attached :pic9
+  has_one_attached :pic10
 end
